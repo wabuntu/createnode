b66728f4b463b07ab41a5d9865350dba7f244eb6
wabuntu <126124899+wabuntu@users.noreply.github.com>
2023-08-18 15:31:12 +0900

    Update createnode.sh

--- createnode.sh
+++ createnode.sh
@@ -9,15 +9,17 @@ NODE=${2}
 
 date;
 
-virt-install --name ${NAME}${NODE} --vcpus 5 --ram 8192 \
+cp focal-server-cloudimg-amd64.img /var/lib/libvirt/images/${NAME}${NODE}-vda.qcow2
+
+virt-install --name ${NAME}${NODE} --vcpus 5 --ram 16384 \
 --osinfo=ubuntu-lts-latest \
 --cpu host-model \
 --pxe --boot network,hd,menu=on --graphics spice \
 --controller scsi,model=virtio-scsi,index=0 \
 --disk path=/var/lib/libvirt/images/${NAME}${NODE}-vda.qcow2,size=32,format=qcow2 \
 --disk path=/var/lib/libvirt/images/${NAME}${NODE}-vdb.qcow2,size=64,format=qcow2 \
---check path_in_use=off \
---network bridge=virbr0,mac=52:54:00:00:86:${NODE},model=virtio \
---network bridge=virbr1,mac=52:54:00:00:87:${NODE},model=virtio \
---check mac_in_use=off \
+--network bridge=virbr0,mac=52:54:00:00:00:${NODE},model=virtio \
+--network bridge=virbr1,mac=52:54:00:00:00:${NODE},model=virtio \
 --cloud-init user-data=user-data
+
+#--noautoconsole
